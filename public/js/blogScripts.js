d2df88a0bf8d376b41acf277a7152aecd30c9ceb
Akash Kumar <akashkumar@Akashs-MacBook-Pro.local>
2021-02-20 23:45:56 -0500

    basically done

---
+++ public/js/blogScripts.js
@@ -0,0 +1,200 @@
+let blogTitle = document.getElementsByTagName("h1")[1].innerText.replace(" ", "-").trim();
+console.log("blog title:" + blogTitle);
+
+
+
+document.getElementById("wink_button").addEventListener('click', function(){
+  let xmlhttp = new XMLHttpRequest();
+
+  // Specify details of the POST request
+  xmlhttp.open("POST", "/blogs/wink/"+blogTitle, true);
+  xmlhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+
+
+
+  // Define the data you’d like to send to the server
+  let postData = {
+   "wink": 1
+  };
+
+  // Make a POST request with your data in the body of the request
+  xmlhttp.send(JSON.stringify(postData)); //postData gets sent to server.js as request.body
+
+
+
+  // Do something once the Response (Good or Bad) has been received
+  xmlhttp.onreadystatechange = function(data) {
+      if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+          let userObject=JSON.parse(xmlhttp.responseText);
+          document.getElementById("wink_count").innerText=userObject.winks;
+      }
+      else {
+
+  	  }
+  }
+});//end of winking
+
+
+
+document.getElementById("comment_button").addEventListener('click', function() {
+  let xmlhttp = new XMLHttpRequest();
+
+  // Specify details of the POST request
+  xmlhttp.open("POST", "/blogs/comments/"+blogTitle, true);
+  xmlhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+
+  let author = document.getElementById('author').value;
+  let content = document.getElementById('content').value;
+
+  let isCommentValid = true;
+  if (content === "") {
+    alert("Comment content may not be an empty string");
+    isCommentValid = false;
+  }
+
+  let divElement = document.createElement("div");
+  divElement.appendChild(document.createElement("hr")); //add horizontal line
+
+  let authorPara = document.createElement('p');
+  let authorText = document.createTextNode("Author: " + author);
+  authorPara.appendChild(authorText);
+  divElement.appendChild(authorPara);
+
+  let datePara = document.createElement('p');
+  let d = new Date();
+  let month = d.getMonth() + 1;
+  if (month == 1) {month = "January"};
+  if (month == 2) {month = "February"};
+  if (month == 3) {month = "March"};
+  if (month == 4) {month = "April"};
+  if (month == 5) {month = "May"};
+  if (month == 6) {month = "June"};
+  if (month == 7) {month = "July"};
+  if (month == 8) {month = "August"};
+  if (month == 9) {month = "September"};
+  if (month == 10) {month = "October"};
+  if (month == 11) {month = "November"};
+  if (month == 12) {month = "December"};
+  let dateNum = d.getDate();
+  let year = d.getFullYear();
+  let date = month + " " + dateNum + ", " + year;
+
+  let dateText = document.createTextNode("Date: " + date);
+  datePara.appendChild(dateText);
+  divElement.appendChild(datePara);
+
+  let contextPara = document.createElement("p");
+  let contextText = document.createTextNode(content);
+  contextPara.appendChild(contextText);
+  divElement.appendChild(contextPara);
+
+  let like_buttons = document.getElementsByClassName("like_button");
+
+  let likePara = document.createElement("p");
+  likePara.innerHTML = "Likes: ";
+  let likeSpan = document.createElement("span");
+  likeSpan.id = "like_count_" + like_buttons.length;
+  likeSpan.innerHTML = 0 + " ";
+  likeSpan.className += "like_count";
+  likePara.appendChild(likeSpan);
+
+  let button = document.createElement("button");
+  button.innerHTML = "Like";
+  button.className += "btn btn-success btn-sm";
+  likePara.appendChild(button);
+  divElement.appendChild(likePara);
+
+  button.addEventListener('click', function(){
+    let xmlhttp = new XMLHttpRequest();
+
+    // Specify details of the POST request
+    xmlhttp.open("POST", "/blogs/likes/"+blogTitle, true);
+    xmlhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+
+    // Define the data you’d like to send to the server
+    let postData = {
+     "likes": 1,
+     "index": like_buttons.length
+    };
+
+    // Make a POST request with your data in the body of the request
+    xmlhttp.send(JSON.stringify(postData)); //postData gets sent to server.js as request.body
+
+    // Do something once the Response (Good or Bad) has been received
+    xmlhttp.onreadystatechange = function(data) {
+        if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+            let userObject = JSON.parse(xmlhttp.responseText);
+            document.getElementById("like_count_" + like_buttons.length).innerText = userObject.comments[like_buttons.length].likes + " ";
+        }
+        else {
+
+        }
+    }
+  });//button event listener
+
+  divElement.appendChild(document.createElement("br")); //add newline
+
+  if (isCommentValid) {
+    document.getElementById('comment-block').appendChild(divElement);
+  }
+
+
+
+  // Define the data you’d like to send to the server
+  let postData = {
+    "author": author,
+    "date": date,
+    "content": content,
+    "isCommentValid": isCommentValid
+  };
+
+  // Make a POST request with your data in the body of the request
+  xmlhttp.send(JSON.stringify(postData)); //postData gets sent to server.js as request.body
+
+  // Do something once the Response (Good or Bad) has been received
+  xmlhttp.onreadystatechange = function(data) {
+      if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+          let userObject = JSON.parse(xmlhttp.responseText);
+          document.getElementById("wink_count").innerText = userObject.winks;
+      }
+      else {
+
+  	  }
+  }
+});//end of commenting
+
+
+  let like_buttons = document.getElementsByClassName("like_button");
+  for (let i = 0; i < like_buttons.length; i++) {
+    let button = like_buttons[i];
+    button.addEventListener('click', function(){
+      let xmlhttp = new XMLHttpRequest();
+
+      // Specify details of the POST request
+      xmlhttp.open("POST", "/blogs/likes/"+blogTitle, true);
+      xmlhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+
+      // Define the data you’d like to send to the server
+      let postData = {
+       "likes": 1,
+       "index": i
+      };
+
+      // Make a POST request with your data in the body of the request
+      xmlhttp.send(JSON.stringify(postData)); //postData gets sent to server.js as request.body
+
+      // Do something once the Response (Good or Bad) has been received
+      xmlhttp.onreadystatechange = function(data) {
+          if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+              let userObject = JSON.parse(xmlhttp.responseText);
+              document.getElementById("like_count_" + i).innerText = userObject.comments[i].likes;
+          }
+          else {
+
+          }
+      }
+    });//end of button event listener
+  }//end of button loop
+/*
+
+*/
--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const ejs = require('ejs');
 //..............Create an Express server object..................//
 const app = express();
 
+const bp = require('body-parser')
+app.use(bp.json())
+app.use(bp.urlencoded({ extended: true }))
+
 //..............Apply Express middleware to the server object....//
 app.use(express.static('public'));
 app.set('views', __dirname + '/views');
@@ -50,7 +54,8 @@ app.get('/blogs/:blog', function(request, response) {
     response.status(404);
     response.setHeader('Content-Type', 'text/html')
     response.render("error", {
-      "errorCode":"404"
+      "errorCode":"404",
+      allPosts: blogs
     });
   }
 });
@@ -66,51 +71,216 @@ app.get('/createBlog', function(request, response) {
 
 
 app.get('/blogs', function(request, response) {
-    let name = request.query.name;
+    let blogs = JSON.parse(fs.readFileSync('data/content.json'));
+    let title = request.query.name;
     let blogTitle = "";
 
     let allPosts = JSON.parse(fs.readFileSync('data/content.json'));
     for (postName in allPosts) {
       let post = allPosts[postName];
-      if(title == post.title){
+      if (title == post.title) {
         blogTitle = title;
         break;
       }
     }
     if (blogTitle) {
-      response.redirect("/user/"+blogTitle);
+      response.redirect("/blogs/"+blogTitle);
     }
     else {
       response.status(404);
       response.setHeader('Content-Type', 'text/html')
       response.render("error", {
-        "errorCode":"404"
+        "errorCode":"404",
+        allPosts: blogs
       });
     }
 });
 
-app.post('/blogs', function(request, response) {
-    let users = JSON.parse(fs.readFileSync('data/content.json'));
 
-    var u = {
-        name: request.body.name.trim(),
-        photo: request.body.photo_link.trim(),
-        wins: 0,
-        losses: 0
+
+app.post('/blogs', function(request, response) {
+    let allPosts = JSON.parse(fs.readFileSync('data/content.json'));
+
+    let d = new Date();
+    let month = d.getMonth() + 1;
+    if (month == 1) {month = "January"};
+    if (month == 2) {month = "February"};
+    if (month == 3) {month = "March"};
+    if (month == 4) {month = "April"};
+    if (month == 5) {month = "May"};
+    if (month == 6) {month = "June"};
+    if (month == 7) {month = "July"};
+    if (month == 8) {month = "August"};
+    if (month == 9) {month = "September"};
+    if (month == 10) {month = "October"};
+    if (month == 11) {month = "November"};
+    if (month == 12) {month = "December"};
+    let dateNum = d.getDate();
+    let year = d.getFullYear();
+    let date = month + " " + dateNum + ", " + year;
+
+    let b = {
+        title: request.body.title.trim().replace(' ', '-'),
+        image: request.body.photoLink.trim(),
+        date: date,
+        author: request.body.author,
+        previewContent: request.body.previewContent,
+        content: [request.body.content1, request.body.content2],
+        tags: [
+          [request.body.tag_content_1, request.body.tag_style_1.toLowerCase()],
+          [request.body.tag_content_2, request.body.tag_style_2.toLowerCase()],
+          [request.body.tag_content_3, request.body.tag_style_3.toLowerCase()]
+        ]
     };
 
-    users[request.body.email.trim()]=u;
-    fs.writeFileSync('data/content.json', JSON.stringify(users));
+    let isBlogValid = true;
 
-    response.redirect("/");
+    if (b.title === "") {
+      isBlogValid = false;
+    }
+    if (b.image === "") {
+      isBlogValid = false;
+    }
+    if (b.previewContent === "") {
+      isBlogValid = false;
+    }
+    for (let i = 0; i < 2; i++) {
+      if (b.content[i] === "") {
+        isBlogValid = false;
+      }
+    }
+    for (let i = 0; i < 3; i++) {
+      if (b.tags[i][0] === "") {
+        isBlogValid = false;
+      }
+    }
+    for (let i = 0; i < 3; i++) {
+      if (b.tags[i][1] === "") {
+        isBlogValid = false;
+      }
+    }
+    if (isBlogValid) {
+      allPosts[request.body.title.trim().replace(' ', '-')] = b;
+      fs.writeFileSync('data/content.json', JSON.stringify(allPosts));
+      response.redirect("/");
+    }
 });
 
+
+
+app.post('/blogs/wink/:blog', function(request, response) {
+  let blogs = JSON.parse(fs.readFileSync('data/content.json'));
+  let blogName = request.params.blog;
+
+  //console.log(request.body);
+
+  if (blogs[blogName]) {
+    if (!blogs[blogName].winks) {
+      blogs[blogName].winks = 0;
+    }
+    blogs[blogName].winks++;
+    fs.writeFileSync('data/content.json', JSON.stringify(blogs));
+
+    response.status(200);
+    response.setHeader('Content-Type', 'text/json');
+    response.send(blogs[blogName]);
+  }
+  else {
+    response.status(404);
+    response.setHeader('Content-Type', 'text/json');
+    response.send('{results: "no user"}');
+  }
+});
+
+
+
+app.post('/blogs/comments/:blog', function(request, response) {
+  let blogs = JSON.parse(fs.readFileSync('data/content.json'));
+  let blogName = request.params.blog;
+
+  //console.log(request.body);
+
+  if (blogs[blogName]) {
+    if (!blogs[blogName].comments) {
+      blogs[blogName].comments = [];
+    }
+
+    let d = new Date();
+    let month = d.getMonth() + 1;
+    if (month == 1) {month = "January"};
+    if (month == 2) {month = "February"};
+    if (month == 3) {month = "March"};
+    if (month == 4) {month = "April"};
+    if (month == 5) {month = "May"};
+    if (month == 6) {month = "June"};
+    if (month == 7) {month = "July"};
+    if (month == 8) {month = "August"};
+    if (month == 9) {month = "September"};
+    if (month == 10) {month = "October"};
+    if (month == 11) {month = "November"};
+    if (month == 12) {month = "December"};
+    let dateNum = d.getDate();
+    let year = d.getFullYear();
+    let date = month + " " + dateNum + ", " + year;
+
+    let c = {
+      author: request.body.author,
+      date: date,
+      content: request.body.content
+    }
+
+    if (request.body.isCommentValid) {
+      blogs[blogName].comments.push(c);
+      fs.writeFileSync('data/content.json', JSON.stringify(blogs));
+    }
+
+    response.status(200);
+    response.setHeader('Content-Type', 'text/json');
+    response.send(blogs[blogName]);
+  }
+  else {
+    response.status(404);
+    response.setHeader('Content-Type', 'text/json');
+    response.send('{results: "no user"}');
+  }
+});
+
+
+
+app.post('/blogs/likes/:blog', function(request, response) {
+  let blogs = JSON.parse(fs.readFileSync('data/content.json'));
+  let blogName = request.params.blog;
+
+  //console.log(request.body);
+
+  if (blogs[blogName]) {
+    if (!blogs[blogName].comments[request.body.index].likes) {
+      blogs[blogName].comments[request.body.index].likes = 0;
+    }
+    blogs[blogName].comments[request.body.index].likes++;
+    fs.writeFileSync('data/content.json', JSON.stringify(blogs));
+
+    response.status(200);
+    response.setHeader('Content-Type', 'text/json');
+    response.send(blogs[blogName]);
+  }
+  else {
+    response.status(404);
+    response.setHeader('Content-Type', 'text/json');
+    response.send('{results: "no user"}');
+  }
+});
+
+
+
 // Because routes/middleware are applied in order, this will act as a default error route in case of an invalid route
-app.use("", function(request, response){
+app.use("", function(request, response) {
+  let blogs = JSON.parse(fs.readFileSync('data/content.json'));
   response.status(404);
   response.setHeader('Content-Type', 'text/html')
   response.render("error", {
-    "errorCode":"404"
+    "errorCode":"404",
+    allPosts: blogs
   });
 });
 
